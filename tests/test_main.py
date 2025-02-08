b633bdf6a67e66cf7d024aceb33874806f66e9aa
luismi4157 <10932238@uvu.edu>
2025-02-08 13:28:29 -0700

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,10 +3,15 @@ import main  # Importing main.py to test its functions
 
 class TestUVSim(unittest.TestCase):
     
-    def test_memory_initialization(self):
+    def test_memory_initialized(self):
         self.assertTrue(main.memory[0] == "")
         main.load_to_memory("tests/Test1.txt")
         self.assertTrue(main.memory[0] != "")
+        
+    def test_memory_not_initialized(self):
+        self.assertTrue(main.memory[0] == "")
+        main.load_to_memory("tests/Test2.txt")
+        self.assertTrue(main.memory[0] == "")
 
 if __name__ == "__main__":
     unittest.main()
