737cb0332ba76651811c0fa92201f4e0bbc393c6
luismi4157 <10932238@uvu.edu>
2025-02-19 20:21:00 -0700

    Initialize memory

--- Model/UVSimModel.py
+++ Model/UVSimModel.py
@@ -1,6 +1,6 @@
 class UVSimModel:
     def __init__(self):
-        self.memory = ["" for _ in range(100)]
+        self.memory = ["+0000" for _ in range(100)]
         self.accumulator = 0
         self.instruction_pointer = 0
     
